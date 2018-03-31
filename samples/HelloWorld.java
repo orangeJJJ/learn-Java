8aaf5214e010310b045ae36a96ab4f81d53b36bd
orangeJJJ <837330442@qq.com>
2018-03-31 19:11:32 +0800

    samples

---
+++ samples/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+  这是什么注释
+  文档注释
+ */
+public class HelloWorld {
+	/*
+	 这是注释
+	 多行注释
+	 */
+
+	public static void main(String[] args) {
+		// 这是单行注释
+		System.out.println("Hello World!");// 单行注释
+	}
+}
